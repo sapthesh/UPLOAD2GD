0801533d6bf3662b2acbe92793061f232ffe013e
Sapthesh V <sapthesh@users.noreply.github.com>
2020-06-10 15:52:52 +0530

    Wrong Username

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -5,7 +5,7 @@ drive_folder_name = "UPLOD2GDbot"
 MEGA_EMAIL = "email"
 MEGA_PASSWORD = "password"
 
-START = "Hi {}  \nI am Upload 2 Google Drive Bot.\nPlease Authorise To use me.\nBy using /auth \n\nFor more info /help\n\nPlease Report Bugs @saptheshv"
+START = "Hi {}  \nI am Upload 2 Google Drive Bot.\nPlease Authorise To use me.\nBy using /auth \n\nFor more info /help\n\nPlease Report Bugs @sapthesh"
 
 HELP = """<b>AUTHORISE BOT</b>
 Use  /auth Command Generate
@@ -19,8 +19,8 @@ command.\n\n
 You Can Upload Any Internet
 Files On Your google
 Drive Account.\n\n
-This Bot is by @saptheshv\n
-Bug Report @saptheshv"""
+This Bot is by @sapthesh\n
+Bug Report @sapthesh"""
 
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
 OL_DOWNLOAD = "Openload Link !! Downloading Started ... \nOpenload Links Are Extremely Slow"
@@ -30,16 +30,16 @@ DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \nMega Links are \nExtremely Slow :("
 DOWN_COMPLETE = "Downloading complete!!"
 NOT_AUTH = "You Are Not Authorised To Using this Bot \n\nPlease Authorise Me Using /auth"
-REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @saptheshv "
+REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @sapthesh"
 AUTH_SUCC = "Authorised Successfully  !! \n\nNow Send me A direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nreport At @saptheshv "
+ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nreport At @sapthesh"
 AUTH_URL = '<a href ="{}">Click Here to Get Auth Code</a>\n\nGenerate And Copy Your Google Drive Token And Send It To Me'
 UPLOADING = "Download Complet !! \nUploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully!! \n\nUse /auth To Re-Authorise Your Drive Acc."
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully.\n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error!! Please Send Me a valid Token or Re-Authorise Me\n\n/auth\n\n report At @saptheshv"
+AUTH_ERROR = "AUTH Error!! Please Send Me a valid Token or Re-Authorise Me\n\n/auth\n\n report At @sapthesh"
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
